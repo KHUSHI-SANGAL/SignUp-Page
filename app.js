da925f325015fe4335fbd89073251f1274f19d59
Khushi Sangal <khushisangal05@gmail.com>
2023-02-27 23:17:14 +0530

    app.js file update

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 
 
 
@@ -39,11 +40,11 @@ app.post("/", function(req, res){
 
   const jsonData = JSON.stringify(data); //js object to flatpack json which we will send to mailchimp
 
-  const url = "https://us11.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}"; //last path is list id becoz in mailchimp we can have multiple lists so we have to specify which list we want to send the data to
+  const url = "https://us11.api.mailchimp.com/3.0/lists/"+process.env.LIST_ID; //last path is list id becoz in mailchimp we can have multiple lists so we have to specify which list we want to send the data to
 
   const options = {        //js object of options
     method: "POST",
-    auth: "${process.env.USERNAME}:${process.env.KEY}"  //username:password
+    auth: process.env.USERNAME + ":" + process.env.KEY  //username:password
   };
 
 
